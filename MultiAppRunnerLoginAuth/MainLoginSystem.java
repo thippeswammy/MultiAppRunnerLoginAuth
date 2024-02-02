ed6645b8f78f57eed7f855b5c18890f6ee7614d9
Thippeswamy k s <73697198+thippeswammy@users.noreply.github.com>
2024-02-02 23:21:43 +0530

    Update MainLoginSystem.java

--- MultiAppRunnerLoginAuth/MainLoginSystem.java
+++ MultiAppRunnerLoginAuth/MainLoginSystem.java
@@ -1,10 +1,3 @@
-package com.thippeswamy.app.MultiAppRunnerLoginAuth;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import javax.swing.*;
 import java.awt.*;
 
